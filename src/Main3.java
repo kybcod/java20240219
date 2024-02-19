a97ccb5cdc1fa8e3402a2b8f9d26f3fba35e2c6f
kyb <kyb-0215@naver.com>
2024-02-19 12:40:42 +0900

    first commit

---
+++ src/Main3.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    public static void main(String[] args){
+        System.out.println("안녕하세요.");
+        System.out.println("저는 김예빈입니다.");
+        System.out.println("저는 24살입니다.");
+        System.out.println("반갑습니다.");
+        System.out.println("잘 부탁드립니다.");
+
+    }
+}
