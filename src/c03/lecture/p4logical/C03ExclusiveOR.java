096b8f8bf80e701807a177d7a962d156132ac03c
kyb <kyb-0215@naver.com>
2024-02-22 17:08:40 +0900

    변수와 타입

---
+++ src/c03/lecture/p4logical/C03ExclusiveOR.java
@@ -0,0 +1,13 @@
+package c03.lecture.p4logical;
+
+public class C03ExclusiveOR {
+    public static void main(String[] args) {
+        //^ : exclusive or (배타적 논리합, XOR)
+        //피연산자 갯수 : 2개, 피연산자 타입/연산결과 타입 : boolean
+        //두 피연산자 값이 다를 때만 true, 같으면 false
+        //true ^ true : false
+        //ture ^ false  : true
+        // false ^ true  : true
+        // false ^ false : false
+    }
+}
